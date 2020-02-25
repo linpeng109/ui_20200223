2cdd49a72b8390eb4d3fb0aa23e916c7b567c576
linpeng109 <linpeng109@aliyun.com>
2020-02-25 17:34:08 +0800

    20200225

--- Open.bat
+++ Open.bat
@@ -1,1 +1,1 @@
-C:\Users\Mars\AppData\Local\UiPath\app-19.10.2\UiRobot.exe execute --file "D:/Workspace/ui_2020_0220/Open.xaml" --input "{'inputfile':'D:/by/图件/巴彦包勒格矿区银多金属矿0号勘查线地质剖面图_分析表.dwf','height':3100,'width':1000}"
+C:\Users\linpe\AppData\Local\UiPath\app-19.10.1\UiRobot.exe execute --file "D:/Workspace/ui_20200223/Open.xaml" --input "{'inputfile':'D:/by/图件/巴彦包勒格矿区银多金属矿0号勘查线地质剖面图_分析表.dwf','height':3100,'width':1000}"
