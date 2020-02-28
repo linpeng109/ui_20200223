288cded5956c40f45f03afbbdf49fcd62e7afc9c
linpeng109 <linpeng109@aliyun.com>
2020-02-28 16:42:44 +0800

    20200228

--- Main.bat
+++ Main.bat
@@ -1,1 +1,6 @@
-C:\Users\linpe\AppData\Local\UiPath\app-19.10.1\UiRobot.exe execute --file "D:/Workspace/ui_20200223/Main.xaml" --input "{'inputfile':'D:/by/图件/巴彦包勒格矿区银多金属矿0号勘查线地质剖面图_分析表.dwf','height':1000,'width':3100}"
+C:\Users\linpe\AppData\Local\UiPath\app-19.10.1\UiRobot.exe execute --file "D:/Workspace/ui_20200223/Open.xaml" --input "{'inputfile':'D:/by/图件/巴彦包勒格矿区银多金属矿0号勘查线地质剖面图_分析表.dwf','height':1000,'width':3100}"
+C:\Users\linpe\AppData\Local\UiPath\app-19.10.1\UiRobot.exe execute --file "D:/Workspace/ui_20200223/Setting.xaml" --input "{'inputfile':'D:/by/图件/巴彦包勒格矿区银多金属矿0号勘查线地质剖面图_分析表.dwf','height':1000,'width':3100}"
+C:\Users\linpe\AppData\Local\UiPath\app-19.10.1\UiRobot.exe execute --file "D:/Workspace/ui_20200223/Print.xaml" --input "{'inputfile':'D:/by/图件/巴彦包勒格矿区银多金属矿0号勘查线地质剖面图_分析表.dwf','height':1000,'width':3100}"
+C:\Users\linpe\AppData\Local\UiPath\app-19.10.1\UiRobot.exe execute --file "D:/Workspace/ui_20200223/Close.xaml" --input "{'inputfile':'D:/by/图件/巴彦包勒格矿区银多金属矿0号勘查线地质剖面图_分析表.dwf','height':1000,'width':3100}"
+
+C:\Users\linpe\AppData\Local\UiPath\app-19.10.1\UiRobot.exe execute --file "D:/Workspace/ui_20200223/CAD.xaml" --input "{'inputfile':'D:/by/图件/巴彦包勒格矿区银多金属矿480m水平断面图','height':3100,'width':1000}"
